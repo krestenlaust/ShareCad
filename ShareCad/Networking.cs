26349687c71ef6dc7cf29e8891a087423b565d52
Kresten Laust <kressckerl@gmail.com>
2021-05-17 11:45:42 +0200

    networking virker næsten (ik kig)

--- ShareCad/Networking.cs
+++ ShareCad/Networking.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,12 @@ namespace ShareCad
 {
     public static class Networking
     {
+        const short PORT = 4040;
+        private static TcpClient tcpClient;
 
+        public static void Connect(IPAddress address)
+        {
+            
+        }
     }
 }
