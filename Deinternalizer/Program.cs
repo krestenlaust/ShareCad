73d00fb1e407c41fbcf398771046325fa77007d2
Kresten Laust <kressckerl@gmail.com>
2021-06-21 19:56:00 +0200

    Deinternalizer værktøj virker muligvis, har ikke testet det

---
+++ Deinternalizer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deinternalizer
+{
+    class Program
+    {
+        // kilder:
+        // https://stackoverflow.com/questions/4971213/how-to-use-reflection-to-determine-if-a-class-is-internal
+
+        static void Main(string[] args)
+        {
+            using (AssemblyDefinition a = AssemblyDefinition.ReadAssembly(args[0], new ReaderParameters { ReadWrite = true }))
+            {
+                foreach (var type in a.Modules[0].Types)
+                {
+                    if (type.IsNestedAssembly)
+                        type.IsPublic = true;
+                }
+                a.Write();
+            }
+        }
+    }
+}
