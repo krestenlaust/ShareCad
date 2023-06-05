2c9c0aa361ed92b59087452e9439e89a9509e2e4
Kresten Laust <krestenlaust@gmail.com>
2023-06-05 12:32:38 +0200

    Included projects in solution and updated framework version

--- ShareCad.Networking.Tests/NetworkManager.cs
+++ ShareCad.Networking.Tests/NetworkManager.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ShareCad.Networking.Tests
 {
